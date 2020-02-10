3a3d67fc37aa1be1fad8ffedd41d2a4517396d43
Carl Riis <carl.aagot.riis@gmail.com>
2020-02-11 00:19:54 +0100

    Basic Island terrain generating

--- src/terrain/HeightsGenerator.java
+++ src/terrain/HeightsGenerator.java
@@ -1,76 +1,151 @@
 
 package terrain;
- 
-import java.util.Random;
- 
-public class HeightsGenerator {
- 
-    private static final float AMPLITUDE = 85f ;
-    private static final int OCTAVES = 5;
-    private static final float ROUGHNESS = 0.25f;
- 
-    private Random random = new Random();
-    private int seed;
-    private int xOffset = 0;
-    private int zOffset = 0;
- 
-    public HeightsGenerator() {
-        this.seed = random.nextInt(1000000000);
-        this.seed = 5;
-    }
-     
-    //only works with POSITIVE gridX and gridZ values!
-    public HeightsGenerator(int gridX, int gridZ, int vertexCount, int seed) {
 
-        this.seed = seed;
-        xOffset = gridX * (vertexCount-1);
-        zOffset = gridZ * (vertexCount-1);
-    }
- 
-    public float generateHeight(int x, int z) {
-        float total = 0;
-        float d = (float) Math.pow(2, OCTAVES-1);
-        for(int i=0;i<OCTAVES;i++){
-            float freq = (float) (Math.pow(2, i) / d);
-            float amp = (float) Math.pow(ROUGHNESS, i) * AMPLITUDE;
-            total += getInterpolatedNoise((x+xOffset)*freq, (z + zOffset)*freq) * amp;
-        }
-        return total;
-    }
-     
-    private float getInterpolatedNoise(float x, float z){
-        int intX = (int) x;
-        int intZ = (int) z;
-        float fracX = x - intX;
-        float fracZ = z - intZ;
-         
-        float v1 = getSmoothNoise(intX, intZ);
-        float v2 = getSmoothNoise(intX + 1, intZ);
-        float v3 = getSmoothNoise(intX, intZ + 1);
-        float v4 = getSmoothNoise(intX + 1, intZ + 1);
-        float i1 = interpolate(v1, v2, fracX);
-        float i2 = interpolate(v3, v4, fracX);
-        return interpolate(i1, i2, fracZ);
-    }
-     
-    private float interpolate(float a, float b, float blend){
-        double theta = blend * Math.PI;
-        float f = (float)(1f - Math.cos(theta)) * 0.5f;
-        return a * (1f - f) + b * f;
-    }
- 
-    private float getSmoothNoise(int x, int z) {
-        float corners = (getNoise(x - 1, z - 1) + getNoise(x + 1, z - 1) + getNoise(x - 1, z + 1)
-                + getNoise(x + 1, z + 1)) / 16f;
-        float sides = (getNoise(x - 1, z) + getNoise(x + 1, z) + getNoise(x, z - 1)
-                + getNoise(x, z + 1)) / 8f;
-        float center = getNoise(x, z) / 4f;
-        return corners + sides + center;
-    }
- 
-    private float getNoise(int x, int z) {
-        random.setSeed(x * 49632 + z * 325176 + seed);
-        return random.nextFloat() * 2f - 1f;
-    }
- 
+import java.util.ArrayList;
+import java.util.Random;
+
+import toolbox.Maths;
+
+public class HeightsGenerator {
+
+	private static final float AMPLITUDE = 85f;
+	private static final int OCTAVES = 5;
+	private static final float ROUGHNESS = 0.25f;
+
+	private static final int ISLAND_CHECK_FREQUENCY = 20;
+	private static final float ISLAND_RADIUS = 160;
+
+	private Random random = new Random();
+	private int seed;
+	private int xOffset = 0;
+	private int zOffset = 0;
+
+	public HeightsGenerator() {
+		this.seed = random.nextInt(1000000000);
+	}
+
+	// only works with POSITIVE gridX and gridZ values!
+	public HeightsGenerator(int gridX, int gridZ, int vertexCount, int seed) {
+		this.seed = seed;
+		xOffset = gridX * (vertexCount - 1);
+		zOffset = gridZ * (vertexCount - 1);
+
+	}
+
+	public float generateHeight(int x, int z) {
+		float total = 0;
+		float d = (float) Math.pow(2, OCTAVES - 1);
+		for (int i = 0; i < OCTAVES; i++) {
+			float freq = (float) (Math.pow(2, i) / d);
+			float amp = (float) Math.pow(ROUGHNESS, i) * AMPLITUDE;
+			total += getInterpolatedNoise((x + xOffset) * freq, (z + zOffset) * freq) * amp;
+		}
+
+		ArrayList<Integer[]> points = getIslandPoints(x, z);
+
+		total += 100;
+
+		float biggestHeight = 0f;
+		for (int i = 0; i < points.size(); i++) {
+
+			float height = total * getDistanceFactor(points.get(i), x, z);
+			if (height > biggestHeight) {
+				biggestHeight = height;
+			}
+		}
+
+		total = biggestHeight;
+		float blendSmoother = 0;
+		int blendSmooths = 0;
+
+		for (int i = 0; i < points.size(); i++) {
+			for (int j = 0; j < points.size(); j++) {
+				if (i != j) {
+
+					float finalHeight = Maths.getSmoothIslandBlendFactor(points.get(i)[0], points.get(i)[1],
+							points.get(j)[0], points.get(j)[1], ISLAND_RADIUS, x, z, total);
+
+					if (finalHeight != 0) {
+						blendSmoother += finalHeight;
+						blendSmooths += 1;
+					}
+				}
+			}
+		}
+
+		if (blendSmooths != 0) {
+			total += (blendSmoother) / blendSmooths;
+		}
+
+		return total;
+	}
+
+	private float getDistanceFactor(Integer[] point, int x, int z) {
+		float distance = (float) Math.sqrt(Math.pow((x - point[0]), 2) + Math.pow((z - point[1]), 2));
+		float distanceFactor = (float) (distance / ISLAND_RADIUS);
+		return (1 - distanceFactor);
+	}
+
+	private float getInterpolatedNoise(float x, float z) {
+		int intX = (int) x;
+		int intZ = (int) z;
+		float fracX = x - intX;
+		float fracZ = z - intZ;
+
+		float v1 = getSmoothNoise(intX, intZ);
+		float v2 = getSmoothNoise(intX + 1, intZ);
+		float v3 = getSmoothNoise(intX, intZ + 1);
+		float v4 = getSmoothNoise(intX + 1, intZ + 1);
+		float i1 = interpolate(v1, v2, fracX);
+		float i2 = interpolate(v3, v4, fracX);
+		return interpolate(i1, i2, fracZ);
+	}
+
+	private float interpolate(float a, float b, float blend) {
+		double theta = blend * Math.PI;
+		float f = (float) (1f - Math.cos(theta)) * 0.5f;
+		return a * (1f - f) + b * f;
+	}
+
+	private float getSmoothNoise(int x, int z) {
+		float corners = (getNoise(x - 1, z - 1) + getNoise(x + 1, z - 1) + getNoise(x - 1, z + 1)
+				+ getNoise(x + 1, z + 1)) / 16f;
+		float sides = (getNoise(x - 1, z) + getNoise(x + 1, z) + getNoise(x, z - 1) + getNoise(x, z + 1)) / 8f;
+		float center = getNoise(x, z) / 4f;
+		return corners + sides + center;
+	}
+
+	private float getNoise(int x, int z) {
+		random.setSeed(x * 49632 + z * 325176 + seed);
+		return random.nextFloat() * 2f - 1f;
+	}
+
+	private float getNoiseForIsland(int x, int z) {
+		random.setSeed((long) (x * 48132 + z * 825236 + seed * 1.3));
+		return random.nextFloat() * 2f - 1f;
+	}
+
+	private ArrayList<Integer[]> getIslandPoints(int x, int z) {
+		ArrayList<Integer[]> points = new ArrayList<Integer[]>();
+
+		int first_pointX = (int) ((ISLAND_CHECK_FREQUENCY - (x % ISLAND_CHECK_FREQUENCY)) + x - ISLAND_RADIUS);
+		int first_pointZ = (int) ((ISLAND_CHECK_FREQUENCY - (z % ISLAND_CHECK_FREQUENCY)) + z - ISLAND_RADIUS);
+
+		for (int iX = 0; iX < Math.floor(((ISLAND_RADIUS * 2) / ISLAND_CHECK_FREQUENCY)); iX++) {
+			for (int iZ = 0; iZ < Math.floor(((ISLAND_RADIUS * 2) / ISLAND_CHECK_FREQUENCY)); iZ++) {
+
+				int pointX = first_pointX + iX * ISLAND_CHECK_FREQUENCY;
+				int pointZ = first_pointZ + iZ * ISLAND_CHECK_FREQUENCY;
+
+
+				// Checking if it's island point
+				if (getNoiseForIsland(pointX, pointZ) > 0.995) {
+					points.add(new Integer[] { pointX, pointZ });
+				}
+			}
+		}
+
+		return points;
+	}
+
 }
--- src/toolbox/Maths.java
+++ src/toolbox/Maths.java
@@ -7,7 +7,7 @@ import org.lwjgl.util.vector.Vector3f;
 import entities.Camera;
 
 public class Maths {
-	
+
 	public static Matrix4f createTransformationMatrix(Vector2f translation, Vector2f scale) {
 		Matrix4f matrix = new Matrix4f();
 		matrix.setIdentity();
@@ -15,7 +15,7 @@ public class Maths {
 		Matrix4f.scale(new Vector3f(scale.x, scale.y, 1f), matrix, matrix);
 		return matrix;
 	}
-	
+
 	public static float barryCentric(Vector3f p1, Vector3f p2, Vector3f p3, Vector2f pos) {
 		float det = (p2.z - p3.z) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.z - p3.z);
 		float l1 = ((p2.z - p3.z) * (pos.x - p3.x) + (p3.x - p2.x) * (pos.y - p3.z)) / det;
@@ -23,29 +23,101 @@ public class Maths {
 		float l3 = 1.0f - l1 - l2;
 		return l1 * p1.y + l2 * p2.y + l3 * p3.y;
 	}
-	
+
 	public static Matrix4f createTransformationMatrix(Vector3f translation, float rx, float ry, float rz, float scale) {
 		Matrix4f matrix = new Matrix4f();
 		matrix.setIdentity();
 		Matrix4f.translate(translation, matrix, matrix);
-		
-		Matrix4f.rotate((float) Math.toRadians(ry), new Vector3f(0,1,0), matrix, matrix);
-		Matrix4f.rotate((float) Math.toRadians(rx), new Vector3f(1,0,0), matrix, matrix);
-		Matrix4f.rotate((float) Math.toRadians(rz), new Vector3f(0,0,1), matrix, matrix);
+
+		Matrix4f.rotate((float) Math.toRadians(ry), new Vector3f(0, 1, 0), matrix, matrix);
+		Matrix4f.rotate((float) Math.toRadians(rx), new Vector3f(1, 0, 0), matrix, matrix);
+		Matrix4f.rotate((float) Math.toRadians(rz), new Vector3f(0, 0, 1), matrix, matrix);
 		Matrix4f.scale(new Vector3f(scale, scale, scale), matrix, matrix);
 		return matrix;
 	}
-	
-	  public static Matrix4f createViewMatrix(Camera camera) {
-	        Matrix4f viewMatrix = new Matrix4f();
-	        viewMatrix.setIdentity();
-	        Matrix4f.rotate((float) Math.toRadians(camera.getPitch()), new Vector3f(1, 0, 0), viewMatrix,
-	                viewMatrix);
-	        Matrix4f.rotate((float) Math.toRadians(camera.getYaw()), new Vector3f(0, 1, 0), viewMatrix, viewMatrix);
-	        Vector3f cameraPos = camera.getPosition();
-	        Vector3f negativeCameraPos = new Vector3f(-cameraPos.x,-cameraPos.y,-cameraPos.z);
-	        Matrix4f.translate(negativeCameraPos, viewMatrix, viewMatrix);
-	        return viewMatrix;
-	    }
+
+	public static Matrix4f createViewMatrix(Camera camera) {
+		Matrix4f viewMatrix = new Matrix4f();
+		viewMatrix.setIdentity();
+		Matrix4f.rotate((float) Math.toRadians(camera.getPitch()), new Vector3f(1, 0, 0), viewMatrix, viewMatrix);
+		Matrix4f.rotate((float) Math.toRadians(camera.getYaw()), new Vector3f(0, 1, 0), viewMatrix, viewMatrix);
+		Vector3f cameraPos = camera.getPosition();
+		Vector3f negativeCameraPos = new Vector3f(-cameraPos.x, -cameraPos.y, -cameraPos.z);
+		Matrix4f.translate(negativeCameraPos, viewMatrix, viewMatrix);
+		return viewMatrix;
+	}
+
+	// Terrain
+
+	public static float getSmoothIslandBlendFactor(float x0, float y0, float x1, float y1, float r, float x, float z,
+			float total) {
+		float d = (float) Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2));
+
+		boolean posible = true;
+		if (d > r * 2) {
+			posible = false;
+		}
+		if (d <= 0) {
+			posible = false;
+		}
+		
+		float finalHeight = 0;
+		
+		if (posible) {
+
+			float a = (float) ((Math.pow(r, 2) - Math.pow(r, 2) + Math.pow(d, 2)) / (d * 2));
+
+			float h = (float) Math.sqrt(Math.pow(r, 2) - Math.pow(a, 2));
+
+			float x2 = x0 + a * (x1 - x0) / d;
+			float y2 = y0 + a * (y1 - y0) / d;
+			float p0x = x2 + h * (y1 - y0) / d;
+			float p0y = y2 - h * (x1 - x0) / d;
+			float p1x = x2 - h * (y1 - y0) / d;
+			float p1y = y2 + h * (x1 - x0) / d;
+
+			float pDistance = pDistance(x, z, p0x, p0y, p1x, p1y);
+			float intersectingWidth = r * 2 - d;
+
+			float factor = ((intersectingWidth / 2) - pDistance) / (intersectingWidth / 2);
+
+			float d0 = (float) Math.sqrt(Math.pow((x - x0), 2) + Math.pow((z - y0), 2));
+			float d1 = (float) Math.sqrt(Math.pow((x - x1), 2) + Math.pow((z - y1), 2));
+
+			if (d0 < r && d1 < r) {
+				float midX = (p0x + p1x) / 2;
+				float midZ = (p0y + p1y) / 2;
+				float dMid = (float) Math.sqrt(Math.pow((x - midX), 2) + Math.pow((z - midZ), 2));
+				float dInter = (float) Math.sqrt(Math.pow((p0x - p1x), 2) + Math.pow((p0y - p1y), 2));
+
+				float dEdge = 0;
+				if (r - d0 < r - d1) {
+					dEdge = r - d0;
+				} else {
+					dEdge = r - d0;
+				}
+				float edgeFactor = (dEdge - 5) / (pDistance + dEdge);
+				float midFactor = 1 - dMid / (dInter);
+				finalHeight = total * edgeFactor * midFactor * factor * 0.5f;
+				if (finalHeight == 0) {
+					finalHeight = total;
+				}
+			}
+		}
+		return finalHeight;
+	}
+
+	private static float pDistance(float x, float y, float x1, float y1, float x2, float y2) {
+		float A = x - x1;
+		float B = y - y1;
+		float C = x2 - x1;
+		float D = y2 - y1;
+		float E = -D;
+		float F = C;
+		float dot = A * E + B * F;
+		float len_sq = E * E + F * F;
+
+		return (float) (Math.abs(dot) / Math.sqrt(len_sq));
+	}
 
 }
